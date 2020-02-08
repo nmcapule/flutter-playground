ce1929b4001bed3b16b15fd350bdf7b8f5537f42
Nathaniel Capule <llss.s.sls.lll@google.com>
2020-02-08 17:45:33 +0800

    Add zephyr

--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+import 'src/editor_page.dart';
 
-class MyApp extends StatelessWidget {
+void main() {
+  runApp(QuickStartApp());
+}
+
+class QuickStartApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
-      ),
-      home: MyHomePage(title: 'Hello flutter'),
+      title: 'Quick Start',
+      home: HomePage(),
+      routes: {
+        "/editor": (context) => EditorPage(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _offsetCounter(offset) {
-    setState(() {
-      _counter += offset;
-    });
-  }
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final navigator = Navigator.of(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: GestureDetector(
-        onVerticalDragUpdate: (details) {
-          _offsetCounter(details.delta.dy);
-        },
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'I heyt drags! Don\'t drag me!',
-                style: TextStyle(fontSize: 30),
-              ),
-              Text(
-                'HUwag mo ako sobokan',
-                style: TextStyle(fontSize: 20),
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headline1,
-              ),
-            ],
-          ),
+      appBar: AppBar(title: Text("Quick Start")),
+      body: Center(
+        child: FlatButton(
+          child: Text("Open editor"),
+          onPressed: () => navigator.pushNamed("/editor"),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
